26584d655f0a47ed7cef1eac99f2398aa64642df
Yangcheng He <107044023+hyc1230@users.noreply.github.com>
2023-06-07 22:55:13 +0800

    Update to 2.1.0

--- LuoguShowEmoji.user.js
+++ LuoguShowEmoji.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Luogu Show Emoji
 // @namespace    blog.heyc.eu.org
-// @version      2.0.4
+// @version      2.1.0
 // @description  Show emoji in Luogu
 // @author       Heyc
 // @match        https://www.luogu.com.cn/*
@@ -18,7 +18,7 @@
 var css = "color: #E67E22;";
 console.log("%c[lgse] Started", css);
 
-var cv = "2.0.4";
+var cv = "2.1.0";
 var reg_lg = [new RegExp("/discuss/[0-9]+"), new RegExp("/user/[0-9]+")];
 
 function chk()
@@ -37,7 +37,7 @@ function chk()
     return false;
 }
 
-var emoji = [
+const emoji = [
     "aini",
     "aiq",
     "am",
@@ -66,6 +66,7 @@ var emoji = [
     "cy",
     "dan",
     "dao",
+    "dax",
     "db",
     "dg",
     "dk",
@@ -77,7 +78,6 @@ var emoji = [
     "ee",
     "emm",
     "fad",
-    "fade",
     "fan",
     "fd",
     "fendou",
@@ -212,7 +212,7 @@ var emoji = [
     "zyj",
 ];
 var re = "(>[^<]*?)(\/%EMOJI%)([^<A-Za-z])";
-var rp = "$1<span style=\"color: #dfdfdf; font-size: 0.3em;\">$2</span><img src=\"%SOURCE%\" alt=\"/%EMOJI%\">$3";
+var rp = "$1<span style=\"color: #dfdfdf; font-size: 0.3em;\">$2</span><img src=\"%SOURCE%\" alt=\"/%EMOJI%\" width=\"28px\" height=\"28px\">$3";
 function run()
 {
     let cmts = [document.querySelectorAll(".am-comment-bd"), document.querySelectorAll(".content")];
@@ -303,8 +303,8 @@ var se_html = `<style>
 </style>
 <script>
 var st = 0;
-var emoji=["aini","aiq","am","azgc","baiy","bangbangt","banzz","baojin","bb","bkx","bl","bp","bq","bs","bt","bu","bz","cd","cg","ch","cha","chi","cj","cp","cs","cy","dan","dao","db","dg","dk","dl","doge","dx","dy","dz","ee","emm","fad","fade","fan","fd","fendou","fj","fn","fw","gg","gy","gz","hanx","haob","hb","hc","hd","hec","hhd","hn","hp","hq","hsh","ht","huaix","hx","jd","jh","jiaybb","jiaybs","jie","jk","jw","jx","jy","ka","kb","kel","kf","kg","kk","kl","kt","kuk","kun","kzht","lb","lengh","lh","ll","lm","lq","lw","lyj","mdfq","mg","mm","ng","nkt","oh","oy","pch","pj","pp","px","pz","qd","qiang","qiao","qidao","qq","qt","ruo","sa","se","sh","shd","shl","shq","shuai","shui","shxi","sr","tiao","tl","tnl","tp","ts","tsh","tt","tuu","tx","ty","wbk","whl","wl","wn","wosl","wq","ws","wul","wx","wzm","xhx","xia","xig","xin","xjj","xk","xs","xu","xw","xy","xyx","yao","yb","yhh","yiw","yl","youl","youtj","yt","yun","yx","zhd","zhem","zhh","zhm","zhq","zj","zk","zq","zt","zuotj","zyj"];
-var emjhtml = '<div class="se-emj"><img src=\"%SOURCE%\" alt=\"/%EMOJI%\"> | %EMOJI%</div>';
+var emoji=["aini","aiq","am","azgc","baiy","bangbangt","banzz","baojin","bb","bkx","bl","bp","bq","bs","bt","bu","bz","cd","cg","ch","cha","chi","cj","cp","cs","cy","dan","dao","dax","db","dg","dk","dl","doge","dx","dy","dz","ee","emm","fad","fan","fd","fendou","fj","fn","fw","gg","gy","gz","hanx","haob","hb","hc","hd","hec","hhd","hn","hp","hq","hsh","ht","huaix","hx","jd","jh","jiaybb","jiaybs","jie","jk","jw","jx","jy","ka","kb","kel","kf","kg","kk","kl","kt","kuk","kun","kzht","lb","lengh","lh","ll","lm","lq","lw","lyj","mdfq","mg","mm","ng","nkt","oh","oy","pch","pj","pp","px","pz","qd","qiang","qiao","qidao","qq","qt","ruo","sa","se","sh","shd","shl","shq","shuai","shui","shxi","sr","tiao","tl","tnl","tp","ts","tsh","tt","tuu","tx","ty","wbk","whl","wl","wn","wosl","wq","ws","wul","wx","wzm","xhx","xia","xig","xin","xjj","xk","xs","xu","xw","xy","xyx","yao","yb","yhh","yiw","yl","youl","youtj","yt","yun","yx","zhd","zhem","zhh","zhm","zhq","zj","zk","zq","zt","zuotj","zyj"];
+var emjhtml = '<div class="se-emj"><img src=\"%SOURCE%\" alt=\"/%EMOJI%\" width="28px" height="28px"> | %EMOJI%</div>';
 function se_cge()
 {
     st = 1 - st;
@@ -328,7 +328,7 @@ function se_srh()
     document.getElementById("se-dsp").innerHTML = ih;
 }
 </script>
-<div class="se-ent" id="se-ent" onclick="%CLICK%" oncontextmenu="window.open('https://lgse.netlify.app/lgse-settings-${cv}.html')" title="右键打开设置"><img src="%SOURCE_CY%"></div>
+<div class="se-ent" id="se-ent" onclick="%CLICK%" oncontextmenu="window.open('https://lgse.netlify.app/lgse-settings-${cv}.html')" title="右键打开设置"><img src="%SOURCE_CY%" width="28px" height="28px"></div>
 <div class="se-mnu" id="se-mnu"><input type="text" class="se-ipt" id="se-ipt" placeholder="搜索表情..." oninput="se_srh()"><div class="se-dsp" id="se-dsp"></div><footer id="se-ftr"></footer></div>`
 function srhemj()
 {
@@ -400,93 +400,72 @@ function upd()
 
 // ------------------------------
 
-var src = ["https://cdn.jsdelivr.net/gh/hyc1230/qqemoji/%EMOJI%.gif", "https://ghproxy.com/https://raw.githubusercontent.com/hyc1230/qqemoji/latest/%EMOJI%.gif", "https://qqemoji.heyc.eu.org/%EMOJI%.gif", "https://qqemoji.netlify.app/%EMOJI%.gif"];
+const src = ["https://cdn.jsdelivr.net/gh/hyc1230/qqemoji/56x56/%EMOJI%.gif", "https://ghproxy.com/https://raw.githubusercontent.com/hyc1230/qqemoji/master/56x56/%EMOJI%.gif", "https://qqemoji.heyc.eu.org/56x56/%EMOJI%.gif", "https://qqemoji.netlify.app/56x56/%EMOJI%.gif"];
 
+const stdef = {
+    "rep-emj": true,
+    "srh-emj": true,
+    "img-src": 0,
+    "chk-upd": true,
+}, stlist = [
+    "rep-emj",
+    "srh-emj",
+    "img-src",
+    "chk-upd",
+];
+var st;
 function readst()
 {
-    let val = GM_getValue("settings", "1\\1\\0\\1");
-    return val;
+    let val = GM_getValue("settings", `{}`);
+    console.log(`%c[lgse] Settings: ${val}`, css);
+    st = JSON.parse(val);
+    for (let i = 0; i < stlist.length; i++)
+        if (st[stlist[i]] == null)
+            st[stlist[i]] = stdef[stlist[i]];
 }
-var st = readst();
-console.log(`%c[lgse] Settings: ${st}`, css);
 
-const ST_NUM = 4;
 function load_lg()
 {
-    try
+    console.log("%c[lgse] Loading settings for Luogu", css);
+    rp = rp.replace(/%SOURCE%/g, src[st["img-src"]]);
+    se_html = se_html.replace(/%SOURCE%/g, src[st["img-src"]]).replace(/%SOURCE_CY%/g, src[st["img-src"]].replace(/%EMOJI%/g, "cy"));
+    if (st["rep-emj"] && chk())
     {
-        console.log("%c[lgse] Loading settings for Luogu", css);
-        let sts = st.split("\\");
-        if (sts.length < ST_NUM)
-        {
-            console.log("%c[lgse] Settings wrong, using default", css);
-            throw "Settings wrong";
-        }
-        rp = rp.replace(/%SOURCE%/g, src[parseInt(sts[2])]);
-        if (parseInt(sts[0]) && chk())
-        {
-            console.log("%c[lgse] Started replacing", css);
-            start();
-        }
-        se_html = se_html.replace(/%SOURCE%/g, src[parseInt(sts[2])]).replace(/%SOURCE_CY%/g, src[parseInt(sts[2])].replace(/%EMOJI%/g, "cy"));
-        if (parseInt(sts[1]))
-        {
-            se_html = se_html.replace(/%CLICK%/g, "se_cge()");
-        }
-        else
-        {
-            se_html = se_html.replace(/%CLICK%/g, `window.open('https://lgse.netlify.app/lgse-settings-${cv}.html')`);
-        }
-        srhemj();
-        if (parseInt(sts[1]) && parseInt(sts[3]))
-        {
-            upd();
-        }
-    }
-    catch (err)
-    {
-        console.log("%c[lgse] Load settings failed, using default", css);
         console.log("%c[lgse] Started replacing", css);
         start();
-        se_html = se_html.replace(/%SOURCE%/g, src[0]).replace(/%SOURCE_CY%/g, src[0].replace(/%EMOJI%/g, "cy")).replace(/%CLICK%/g, "se_cge()");
-        srhemj();
+    }
+    if (st["srh-emj"])
+    {
+        se_html = se_html.replace(/%CLICK%/g, "se_cge()");
+    }
+    else
+    {
+        se_html = se_html.replace(/%CLICK%/g, `window.open('https://lgse.netlify.app/lgse-settings-${cv}.html')`);
+    }
+    srhemj();
+    if (st["srh-emj"] && st["chk-upd"])
+    {
         upd();
     }
 }
 function load_st()
 {
-    try
-    {
-        console.log("%c[lgse] Loading settings for Settings page", css);
-        let sts = st.split("\\");
-        if (sts.length >= ST_NUM)
-        {
-            document.getElementById("rep-emj").checked = parseInt(sts[0]);
-            document.getElementById("srh-emj").checked = parseInt(sts[1]);
-            document.getElementById("img-src").selectedIndex = parseInt(sts[2]);
-            document.getElementById("chk-upd").checked = parseInt(sts[3]);
-        }
-        else
-        {
-            console.log("%c[lgse] Settings wrong, using default", css);
-        }
-        document.getElementById("status").style.color = "#52c41a";
-        document.getElementById("status").innerText = "加载完成";
-        document.getElementById("info").innerText = st;
-    }
-    catch (err)
-    {
-        console.log(`%c[lgse] ${err}`, css);
-        throw "In load_st: Setting load failed";
-    }
+    console.log("%c[lgse] Loading settings for Settings page", css);
+    document.getElementById("rep-emj").checked = st["rep-emj"];
+    document.getElementById("srh-emj").checked = st["srh-emj"];
+    document.getElementById("img-src").selectedIndex = st["img-src"];
+    document.getElementById("chk-upd").checked = st["chk-upd"];
+    document.getElementById("status").style.color = "#52c41a";
+    document.getElementById("status").innerText = "加载完成";
+    document.getElementById("info").innerText = JSON.stringify(st);
 }
 
 function listen_st()
 {
     let stn = document.getElementById("info").innerText;
-    if (stn != "" && stn != st)
+    if (stn != "" && stn != JSON.stringify(st))
     {
-        st = stn;
+        st = JSON.parse(stn);
         GM_setValue("settings", stn);
         document.getElementById("status").style.color = "#52c41a";
         document.getElementById("status").innerText = "已保存";
@@ -495,22 +474,21 @@ function listen_st()
     setInterval(listen_st, 1000);
 }
 
-if (document.location.hostname == "www.luogu.com.cn")
+try
 {
-    load_lg();
-}
-else
-{
-    try
+    readst();
+    if (document.location.hostname == "www.luogu.com.cn")
+    {
+        load_lg();
+    }
+    else
     {
         load_st();
         listen_st();
     }
-    catch (err)
-    {
-        document.getElementById("status").style.color = "#e74c3c";
-        document.getElementById("status").innerText = "脚本出现错误";
-        console.log(`%c[lgse] Fatal error: ${err}`, css);
-        console.log("%c[lgse] Terminating", css);
-    }
+}
+catch (err)
+{
+    console.log(`%c[lgse] ERROR ${err}`, css);
+    console.log("%c[lgse] Crash", css);
 }
