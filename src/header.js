680428a1b565521a654f06a26e9a3519b96710ab
hyc1230 <i@heyc.eu.org>
2023-08-31 22:41:59 +0800

    fix: 修复 jQuery 静态文件源

--- src/header.js
+++ src/header.js
@@ -7,7 +7,7 @@
 // @match        https://www.luogu.com.cn/*
 // @match        *://*/*lgse-settings*
 // @icon         https://lgse.heyc.eu.org/favicon.ico
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      https://ajax.aspnetcdn.com/ajax/jquery/jquery-3.6.0.min.js
 // @require      https://unpkg.com/marked/marked.min.js
 // @connect      lgse-source.heyc.eu.org
 // @connect      lgse-source.netlify.app
